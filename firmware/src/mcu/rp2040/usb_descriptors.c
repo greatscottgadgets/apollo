52658c4cde42ca2519ae4112e8114a4fdf31e7ea
marble <git@computer-in.love>
2024-06-11 22:02:54 +0200

    firmware: add copy usb descriptor code from samd11

--- firmware/src/mcu/rp2040/usb_descriptors.c
+++ firmware/src/mcu/rp2040/usb_descriptors.c
@@ -33,15 +33,15 @@
 
 #define SERIAL_NUMBER_STRING_INDEX 3
 
-/* A combination of interfaces must have a unique product id, since PC will save device driver after the first plug.
- * Same VID/PID with different interface e.g MSC (first), then CDC (later) will possibly cause system error on PC.
- *
- * Auto ProductID layout's Bitmap:
- *   [MSB]       MIDI | HID | MSC | CDC          [LSB]
- */
-#define _PID_MAP(itf, n)  ( (CFG_TUD_##itf) << (n) )
-#define USB_PID           (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | \
-													 _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4) )
+// /* A combination of interfaces must have a unique product id, since PC will save device driver after the first plug.
+//  * Same VID/PID with different interface e.g MSC (first), then CDC (later) will possibly cause system error on PC.
+//  *
+//  * Auto ProductID layout's Bitmap:
+//  *   [MSB]       MIDI | HID | MSC | CDC          [LSB]
+//  */
+// #define _PID_MAP(itf, n)  ( (CFG_TUD_##itf) << (n) )
+// #define USB_PID           (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | \
+// 													 _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4) )
 
 //--------------------------------------------------------------------+
 // Device Descriptors
@@ -52,11 +52,12 @@ tusb_desc_device_t const desc_device =
 	.bDescriptorType    = TUSB_DESC_DEVICE,
 	.bcdUSB             = 0x0200,
 
-	// Use Interface Association Descriptor (IAD) for CDC
-	// As required by USB Specs IAD's subclass must be common class (2) and protocol must be IAD (1)
-	.bDeviceClass       = TUSB_CLASS_MISC,
-	.bDeviceSubClass    = MISC_SUBCLASS_COMMON,
-	.bDeviceProtocol    = MISC_PROTOCOL_IAD,
+	// We use bDeviceClass = 0 to indicate that we're a composite device.
+	// Another option is to use the Interface Association Descriptor (IAD) method, 
+	// but this requires extra descriptors.
+	.bDeviceClass       = 0,
+	.bDeviceSubClass    = 0,
+	.bDeviceProtocol    = 0,
 
 	.bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
 
@@ -93,41 +94,19 @@ enum
 
 #define CONFIG_TOTAL_LEN    (TUD_CONFIG_DESC_LEN + TUD_CDC_DESC_LEN + TUD_DFU_RT_DESC_LEN)
 
-#if CFG_TUSB_MCU == OPT_MCU_LPC175X_6X || CFG_TUSB_MCU == OPT_MCU_LPC177X_8X || CFG_TUSB_MCU == OPT_MCU_LPC40XX
-	// LPC 17xx and 40xx endpoint type (bulk/interrupt/iso) are fixed by its number
-	// 0 control, 1 In, 2 Bulk, 3 Iso, 4 In etc ...
-	#define EPNUM_CDC_NOTIF   0x81
-	#define EPNUM_CDC_DATA    0x02
-#else
-	#define EPNUM_CDC_NOTIF   0x81
-	#define EPNUM_CDC_DATA    0x02
-#endif
 
-uint8_t const desc_fs_configuration[] =
+uint8_t const desc_configuration[] =
 {
 	// Interface count, string index, total length, attribute, power in mA
 	TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
 
 	// Interface number, string index, EP notification address and size, EP data address (out, in) and size.
-	TUD_CDC_DESCRIPTOR(ITF_NUM_CDC, 4, EPNUM_CDC_NOTIF, 8, EPNUM_CDC_DATA, 0x80 | EPNUM_CDC_DATA, 64),
+	TUD_CDC_DESCRIPTOR(ITF_NUM_CDC, 0, 0x81, 8, 0x02, 0x83, 64),
 
 	// Interface descriptor for the DFU runtime interface.
-	TUD_DFU_RT_DESCRIPTOR(ITF_NUM_DFU_RT, 5, 0x0d, 500, 4096),
+	TUD_DFU_RT_DESCRIPTOR(ITF_NUM_DFU_RT, 0, 0x0d, 500, 4096),
 };
 
-#if TUD_OPT_HIGH_SPEED
-uint8_t const desc_hs_configuration[] =
-{
-	// Config number, interface count, string index, total length, attribute, power in mA
-	TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
-
-	// 1st CDC: Interface number, string index, EP notification address and size, EP data address (out, in) and size.
-	TUD_CDC_DESCRIPTOR(ITF_NUM_CDC, 5, EPNUM_CDC_NOTIF, 8, EPNUM_CDC_DATA, 0x80 | EPNUM_CDC_DATA, 512),
-
-	// Interface number, string index, attributes, detach timeout, transfer size */
-	TUD_DFU_RT_DESCRIPTOR(ITF_NUM_DFU_RT, 4, 0x0d, 1000, 4096),
-};
-#endif
 
 // Invoked when received GET CONFIGURATION DESCRIPTOR
 // Application return pointer to descriptor
@@ -135,12 +114,7 @@ uint8_t const desc_hs_configuration[] =
 uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
 {
 	(void) index; // for multiple configurations
-#if TUD_OPT_HIGH_SPEED
-	// Although we are highspeed, host may be fullspeed.
-	return (tud_speed_get() == TUSB_SPEED_HIGH) ?  desc_hs_configuration : desc_fs_configuration;
-#else
-	return desc_fs_configuration;
-#endif
+	return desc_configuration;
 }
 
 //--------------------------------------------------------------------+
@@ -153,10 +127,8 @@ char const* string_desc_arr [] =
 {
 	(const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
 	"Great Scott Gadgets",         // 1: Manufacturer
-	"LUNA Debug Controller",       // 2: Product
+	"Apollo Debugger",             // 2: Product
 	NULL,                          // 3: Serials, should use chip ID
-	"UART Bridge",                 // 4: CDC Interface
-	"DFU Runtime"                  // 5: DFU Interface
 };
 
 static uint16_t _desc_str[34];
@@ -173,6 +145,10 @@ static uint16_t *get_serial_number_string_descriptor(void)
 	const unsigned serial_number_chars = PICO_UNIQUE_BOARD_ID_SIZE_BYTES*2;
 
 	int count = 0;
+
+	//
+	// Read and save the device serial number as hexadecimal.
+	//
 
 	if (!got_unique_board_id) {
 		pico_get_unique_board_id(&uID);
@@ -211,11 +187,17 @@ uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid)
 	// Otherwise, take the ASCII string provided and encode it as UTF-16.
 	else {
 
-		if ( !(index < sizeof(string_desc_arr)/sizeof(string_desc_arr[0])) ) {
-			return NULL;
-		}
+		const char* str;
+		if (index == 0xee) {
+			// Microsoft OS 1.0 String Descriptor
+			str = "MSFT100\xee";
+		} else {
+			if ( !(index < sizeof(string_desc_arr)/sizeof(string_desc_arr[0])) ) {
+				return NULL;
+			}
 
-		const char* str = string_desc_arr[index];
+			str = string_desc_arr[index];
+		}
 
 		// Cap at max char
 		chr_count = strlen(str);
